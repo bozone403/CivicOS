f82ba72e7aecbedbc471b5ca53d1dd5266296fb1
Jordan Boisclair <jordan@MacBook-Air-Jordan.local>
2025-08-01 12:52:56 -0600

    ENABLE: Contacts routes for officials endpoint

--- server/appRoutes.ts
+++ server/appRoutes.ts
@@ -22,7 +22,7 @@ import { registerPoliticiansRoutes } from "./routes/politicians.js";
 import { registerBillsRoutes } from "./routes/bills.js";
 import { registerNewsRoutes } from "./routes/news.js";
 import { registerFinanceRoutes } from "./routes/finance.js";
-// import { registerContactsRoutes } from "./routes/contacts.js"; // Temporarily disabled
+import { registerContactsRoutes } from "./routes/contacts.js";
 import { registerLegalRoutes } from "./routes/legal.js";
 import { registerMapsRoutes } from "./routes/maps.js";
 import { registerProcurementRoutes } from "./routes/procurement.js";
@@ -59,7 +59,7 @@ export async function registerRoutes(app: Express): Promise<void> {
   registerBillsRoutes(app);
   registerNewsRoutes(app);
   registerFinanceRoutes(app);
-  // registerContactsRoutes(app); // Temporarily disabled
+  registerContactsRoutes(app);
   registerLegalRoutes(app);
   registerMapsRoutes(app);
   registerProcurementRoutes(app);
